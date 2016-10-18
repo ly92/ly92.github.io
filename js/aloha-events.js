30f40bfc5b2ca701404470dcdcc50ab1272e7ac2
ly <1364757394@qq.com>
2016-10-18 18:19:36 +0800

    Site updated: 2016-10-18 18:19:36

---
+++ js/aloha-events.js
@@ -0,0 +1,14 @@
+$(function () {
+    $("#menu-icon").on("click", function () {
+        $('#menu-sidebar')
+            .sidebar('setting', 'transition', 'overlay')
+            .sidebar('toggle');
+    });
+    $("#right-menu-icon").on("click", function () {
+        $('#right-menu-sidebar')
+            .sidebar('setting', 'transition', 'overlay')
+            .sidebar('toggle');
+        ;
+    });
+    // $(".magnific-img").magnificPopup({type: 'image'});
+})
